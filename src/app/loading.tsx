4603a432311b69fbb46d73d75db1b7ed58a6d355
DevMuktary <mukhtarakanniabdulwaheed@gmail.com>
2025-12-21 08:06:45 +0100

    Create loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import GlobalLoader from '@/components/GlobalLoader';
+
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
+      <GlobalLoader />
+    </div>
+  );
+}
