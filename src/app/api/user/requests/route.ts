56c668509b7a163d619b96a1d5c3e69bb449c706
DevMuktary <mukhtarakanniabdulwaheed@gmail.com>
2026-01-26 14:50:27 +0100

    Update route.ts

--- src/app/api/user/requests/route.ts
+++ src/app/api/user/requests/route.ts
@@ -21,7 +21,7 @@ export async function GET(req: Request) {
         ...(type && { serviceType: type as any }) // Filter if type provided
       },
       orderBy: { createdAt: 'desc' },
-      take: 50OO
+      take: 5000
     });
 
     return NextResponse.json(requests);
